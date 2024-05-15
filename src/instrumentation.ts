bd625e4bba6340ddaf617851e8ad2e51c09539f8
AlivkaRadkivska <134500026+AlivkaRadkivska@users.noreply.github.com>
2024-05-15 23:25:25 +0300

    add sentry loggin and eslint configs

---
+++ src/instrumentation.ts
@@ -0,0 +1,20 @@
+import * as Sentry from '@sentry/nextjs';
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    Sentry.init({
+      dsn: 'https://09cd2d792287f62931a6bca963ba0f31@o4507261939548160.ingest.de.sentry.io/4507261950296144',
+      tracesSampleRate: 1,
+      debug: false,
+      // spotlight: process.env.NODE_ENV === 'development',
+    });
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    Sentry.init({
+      dsn: 'https://09cd2d792287f62931a6bca963ba0f31@o4507261939548160.ingest.de.sentry.io/4507261950296144',
+      tracesSampleRate: 1,
+      debug: false,
+    });
+  }
+}
